acf83b35cea07d2ef149fec50c74fb3b3996f0f1
wjb <wjb@wjbdeMacBook-Pro.local>
2021-02-21 23:43:59 +0800

    fix:aa

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-/*
- * @Author: your name
- * @Date: 2021-02-19 17:14:52
- * @LastEditTime: 2021-02-19 17:32:17
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /ecshopx-newpc/Users/wujiabao/Desktop/work/express-practice/index.js
- */
 const express = require('express')
-const app = express() 
+const app = express()  
 
-app.use('/static', express.static('public'))
-
-const port = 9009;
+const port = 9000;
 
 app.get('/', function (req, res) {
     res.send('Got a Get request')
